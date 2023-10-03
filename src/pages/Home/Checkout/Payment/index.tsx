0935d8b05c71ffece05fa56f765b6b836c37230d
gdegato <rosele.tst@gmail.com>
2023-10-03 18:26:37 -0300

    Refatorand o componente Payment

--- src/pages/Home/Checkout/Payment/index.tsx
+++ src/pages/Home/Checkout/Payment/index.tsx
@@ -1,9 +1,14 @@
 import { Bank, CreditCard, CurrencyDollarSimple, Money } from 'phosphor-react'
-import { ContainerPayment } from './styles'
-import { Button } from '../../../../components/Button'
+import { PaymentButton, ContainerPayment } from './styles'
+import { useState } from 'react'
 
 export function Payment() {
-    
+
+    const [buttonChecked, setButtonChecked] = useState('credit card')
+
+    function handleChange(payment: string) {
+        setButtonChecked(payment)
+    }
     return (
         <ContainerPayment>
             <div className='payment-title'>
@@ -11,15 +16,56 @@ export function Payment() {
                 <p>O pagamento é feito na entrega. Escolha a forma que deseja pagar</p>
             </div>
             <div className='payment-buttons'>
-                <Button text='cartão de crédito'>
-                    <CreditCard size={16} color='#8047F8' />
-                </Button>
-                <Button text='cartão de débito'>
-                    <Bank size={16} color='#8047F8' />
-                </Button>
-                <Button text='dinheiro'>
-                    <Money size={16} color='#8047F8' />
-                </Button>
+
+                <PaymentButton
+                    htmlFor="credit card"
+                    color='#8047F8'
+                    checked={buttonChecked === 'credit card'}
+                >
+                    <CreditCard size={22} />
+                    <p>Cartão de crédito</p>
+                    <input
+                        type="radio"
+                        id="credit card"
+                        value="credit card"
+
+                        name="payment"
+                        onChange={() => handleChange('credit card')}
+                    />
+                </PaymentButton>
+
+                <PaymentButton
+                    htmlFor="debit card"
+                    color='#8047F8'
+                    checked={buttonChecked === 'debit card'}
+                >
+                    <Bank size={22} />
+                    <p>Cartão de débito</p>
+                    <input
+                        type="radio"
+                        id="debit card"
+                        value="debit card"
+
+                        onChange={() => handleChange('debit card')}
+                        name="payment"
+                    />
+                </PaymentButton>
+
+                <PaymentButton
+                    htmlFor="money"
+                    color='#8047F8'
+                    checked={buttonChecked === 'money'}>
+                    <Money size={22} />
+                    <p>Dinheiro</p>
+                    <input
+                        type="radio"
+                        id="money"
+                        value="money"
+
+                        onChange={() => handleChange('money')}
+                        name="payment"
+                    />
+                </PaymentButton>
             </div>
         </ContainerPayment>
 
--- src/pages/Home/Checkout/Payment/styles.ts
+++ src/pages/Home/Checkout/Payment/styles.ts
@@ -1,5 +1,9 @@
 import styled from 'styled-components'
 
+interface PaymentButtonProps {
+  checked: boolean
+}
+
 export const ContainerPayment = styled.section`
   margin-top: 40px;
   width: 662px;
@@ -27,11 +31,65 @@ export const ContainerPayment = styled.section`
       margin-bottom: 28px;
     }
   }
-
   .payment-buttons {
     margin: 0 auto;
     display: flex;
     flex-direction: row;
     justify-content: space-around;
+  }
+`
+
+export const PaymentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  height: 13rem;
+  background-color: ${(props) => props.theme['base-card']};
+  margin-top: 1rem;
+  padding: 2.5rem;
+  border-radius: 8px;
+  font-size: 1.32rem;
+`
+export const PaymentButtonsContainer = styled.div`
+  display: flex;
+  gap: 1rem;
+`
+export const PaymentButton = styled.label<PaymentButtonProps>`
+  width: 11.18rem;
+  height: 3.2rem;
+  border: none;
+  border-radius: 8px;
+  background-color: ${(props) =>
+    props.checked ? props.theme['purple-light'] : props.theme['base-button']};
+  box-shadow: ${(props) =>
+    props.checked ? '0 0 0 2px' + props.theme.purple : 'none'};
+  color: ${(props) => props.theme.purple};
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: left;
+  gap: 0.5rem;
+  padding-left: 0.7rem;
+
+  transition: background-color 0.5s ease-in-out;
+
+  p {
+    color: ${(props) => props.theme['base-subtitle']};
+    font-size: 0.75rem;
+    text-transform: uppercase;
+  }
+
+  :hover {
+    background-color: ${(props) => props.theme['base-hover']};
+  }
+
+  :focus {
+    background-color: ${(props) => props.theme['purple-light']};
+    box-shadow: 0 0 0 2px ${(props) => props.theme.purple};
+  }
+  
+  input {
+    opacity: 0;
   }
 `
